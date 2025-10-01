ae3bcab9b10145bfb1f5a7a5a4a4a32d2758e4a2
charangoud2004 <k.charangoud2004@gmail.com>
2025-10-01 20:50:13 +0530

    p6

--- day1.java
+++ day1.java
@@ -81,3 +81,20 @@ class p5 {
     return false;
   }
 }
+
+class p6 {
+  public boolean isPrime(int n) {
+    // your code goes here
+    int counter = 0;
+    for (int i = 1; i <= n; i++) {
+      if (n % i == 0) {
+        counter++;
+      }
+    }
+    if (counter == 2) {
+      return true;
+    }
+    return false;
+  }
+}
+
