05993cdce9f486f7b53f8dcea8b14372760aa5b7
charangoud2004 <k.charangoud2004@gmail.com>
2025-10-01 22:08:17 +0530

    p8

--- day1.java
+++ day1.java
@@ -1,6 +1,5 @@
 class p1 {
     public int countDigit(int n) {
-
        int counter = 0;
        for(; n >0 ; n/=10){
          counter++;
@@ -22,20 +21,15 @@ class p2 {
 
 class p3 {
   public boolean isPalindrome(int n) {
-
     int rev = 0;
     int check = n;
-
     for (; n > 0; n /= 10) {
       int store = n % 10;
-
       rev = rev * 10 + store;
     }
-
     if (rev == check) {
       return true;
     }
-
     return false;
   }
 }
@@ -44,7 +38,6 @@ class p4{
   public int GCD(int n1, int n2) {
     n1 = Math.abs(n1);
     n2 = Math.abs(n2);
-
     // Euclidean algorithm 1
     while (n2 != 0) {
       int temp = n2;
@@ -57,27 +50,21 @@ class p4{
 
 class p5 {
   public boolean isArmstrong(int n) {
-
     int digit = 0;
     int numSum = 0;
     int compare = n;
     int comp = n;
-
     for (; comp > 0; comp /= 10) {
       digit++;
     }
-
     for (; n > 0; n /= 10) {
       int Num = n % 10;
       Num = (int) Math.pow(Num, digit);
-
       numSum = Num + numSum;
     }
-
     if (numSum == compare) {
       return true;
     }
-
     return false;
   }
 }
@@ -99,12 +86,16 @@ class p6 {
 }
 
 class p7 {
-
   public void printNumbers(int n) {
     if (n == 0) return;
     System.out.println(n);
-
     printNumbers(n - 1);
   }
 }
 
+class p8 {
+  public int NnumbersSum(int N) {
+    if (N == 0) return 0;
+    return N + NnumbersSum(N - 1);
+  }
+}
