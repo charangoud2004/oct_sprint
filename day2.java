ac2bdacba59d277c4eb0c7d6d74248236799ba06
charangoud2004 <k.charangoud2004@gmail.com>
2025-10-02 22:49:57 +0530

    bubble sort

---
+++ day2.java
@@ -0,0 +1,15 @@
+class p1 {
+  public int[] bubbleSort(int[] nums) {
+    for (int i = 0; i < nums.length - 1; i++) {
+      for (int j = 0; j < nums.length - i - 1; j++) {
+        if (nums[j] >= nums[j + 1]) {
+          int tmp = nums[j];
+          nums[j] = nums[j + 1];
+          nums[j + 1] = tmp;
+        }
+      }
+    }
+    return nums;
+  }
+}
+
