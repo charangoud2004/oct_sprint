5c67ca0af3629ae0f5c891a48db4d90c2ff78e07
charangoud2004 <k.charangoud2004@gmail.com>
2025-10-03 22:26:58 +0530

    2nd largest element in array

--- day3.java
+++ day3.java
@@ -16,3 +16,24 @@ class p1 {
   }
 }
 
+class p2 {
+  public int secondLargestElement(int[] nums) {
+    int max = nums[0];
+    int max2 = Integer.MIN_VALUE;
+    for (int i = 0; i < nums.length; i++) {
+      if (max < nums[i]) {
+        max = nums[i];
+      }
+    }
+    for (int i = 0; i < nums.length; i++) {
+      if (max2 < nums[i] && nums[i] < max) {
+        max2 = nums[i];
+      }
+    }
+    if (max2 == Integer.MIN_VALUE) {
+      return -1;
+    }
+    return max2;
+  }
+}
+
