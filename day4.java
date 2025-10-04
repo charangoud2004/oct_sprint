7f14dd11ddf0606b82cc2c5e92c273130fec792d
charangoud2004 <k.charangoud2004@gmail.com>
2025-10-04 22:48:52 +0530

    reversal algo

--- day4.java
+++ day4.java
@@ -27,3 +27,29 @@ class Solution {
         nums[nums.length - 1] = firstElement;
     }
 }
+
+class p3 {
+    public void rotateLeft(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n; // handle cases when k >= n
+
+        // Step 1: Reverse first k elements
+        reverse(nums, 0, k - 1);
+
+        // Step 2: Reverse remaining n-k elements
+        reverse(nums, k, n - 1);
+
+        // Step 3: Reverse whole array
+        reverse(nums, 0, n - 1);
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
