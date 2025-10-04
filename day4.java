fea2e3b20b891db02f8f96cfa8c3c31637bf4a92
charangoud2004 <k.charangoud2004@gmail.com>
2025-10-04 21:54:51 +0530

    rotation of array

--- day4.java
+++ day4.java
@@ -12,3 +12,18 @@ class p1 {
     return left + 1;
   }
 }
+
+class Solution {
+    public void rotateArrayByOne(int[] nums) {
+        // Handle edge cases
+        if (nums.length <= 1) return;
+        // Store the first element
+        int firstElement = nums[0];
+        // Shift elements to the left by one position
+        for (int i = 1; i < nums.length; i++) {
+            nums[i - 1] = nums[i];
+        }
+        // Place the first element at the end
+        nums[nums.length - 1] = firstElement;
+    }
+}
