7542115bd635e8895c1e2b1d4ed39f27bf39f8af
charangoud2004 <k.charangoud2004@gmail.com>
2025-10-06 22:35:00 +0530

    liner search

--- day6.java
+++ day6.java
@@ -70,3 +70,15 @@ class maxConsecutiveOnes {
   }
 }
 
+class linear_Search {
+  public int linearSearch(int nums[], int target) {
+    
+    for (int i = 0; i < nums.length; i++) {
+      if (target == nums[i]) {
+        return i;
+      }
+    }
+    return -1;
+  }
+}
+
