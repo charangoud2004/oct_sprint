ae098235f23ee2930ddda9373365d9e2a87276b7
charangoud2004 <k.charangoud2004@gmail.com>
2025-10-08 22:11:52 +0530

    single time itrated value in duplicated array

---
+++ day8.java
@@ -0,0 +1,16 @@
+class singleNumber_inDuplicates {
+  public int singleNumber(int[] nums) {
+    for (int i = 0; i < nums.length; i++) {
+      int counter = 0;
+      for (int j = 0; j < nums.length; j++) {
+        if (nums[i] == nums[j]) {
+          counter++;
+        }
+      }
+      if (counter == 1) {
+        return nums[i];
+      }
+    }
+    return 0;
+  }
+}
