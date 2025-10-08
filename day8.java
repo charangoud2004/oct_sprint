1c8ce7bb2d1907912e86811b3264c970cf6f485b
charangoud2004 <k.charangoud2004@gmail.com>
2025-10-08 23:15:07 +0530

    classic two sum problem

--- day8.java
+++ day8.java
@@ -33,3 +33,16 @@ class longestSubarray_withSumK {
     return maxLen;
   }
 }
+
+class classicTwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {   // j starts at i+1
+                if (nums[i] + nums[j] == target) {
+                return new int[] {i, j};
+                }
+            }
+        }
+        return null;                    
+    }
+}
