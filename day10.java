14350552f02c111a19dd3086abd174895527936c
charangoud2004 <k.charangoud2004@gmail.com>
2025-10-10 22:05:50 +0530

    max sum in the subArray

---
+++ day10.java
@@ -0,0 +1,21 @@
+class p1 {
+    public int maxSubArray(int[] nums) {
+        long maxi = Long.MIN_VALUE; // maximum sum
+        long sum = 0;
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+            if (sum > maxi) {
+                maxi = sum;
+            }
+            // If sum < 0: discard the sum calculated
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+        // To consider the sum of the empty subarray
+        // uncomment the following check:
+        // if (maxi < 0) maxi = 0;
+        return (int) maxi;
+    }
+}
