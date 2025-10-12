02927f7c1ecfb6b9dfd22d5098808aebbb45c51c
charangoud2004 <k.charangoud2004@gmail.com>
2025-10-12 21:57:04 +0530

    numbers in order

---
+++ day12.java
@@ -0,0 +1,34 @@
+class numbers_in_order {
+  public int longestConsecutive(int[] nums) {
+    // Arrays.sort(nums);
+    for (int i = 0; i < nums.length - 1; i++) {
+      int small = i;
+      for (int j = i + 1; j < nums.length; j++) {
+        if (nums[small] > nums[j]) {
+          small = j;
+        }
+      }
+      int temp = nums[small];
+      nums[small] = nums[i];
+      nums[i] = temp;
+    }
+
+    if (nums.length < 2) {
+      return nums.length;
+    }
+
+    int maxCounter = 1;
+    int counter = 1;
+    for (int k = 0; k < nums.length - 1; k++) {
+      if (nums[k] == nums[k + 1]) {
+        continue;
+      }
+      if (nums[k] + 1 == nums[k + 1]) {
+        counter++;
+      }
+      maxCounter = Math.max(maxCounter, counter);
+    }
+
+    return maxCounter;
+  }
+}
