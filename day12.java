fd85199e2e0f061d79b98431a6222269ce7113c8
charangoud2004 <k.charangoud2004@gmail.com>
2025-10-12 22:19:36 +0530

    Subarray Sum Equals K

--- day12.java
+++ day12.java
@@ -32,3 +32,19 @@ class numbers_in_order {
     return maxCounter;
   }
 }
+
+class Subarray_Sum_Equals_K {
+    public int subarraySum(int[] nums, int k) {
+        int counter = 0;
+        for (int i = 0; i < nums.length ; i++) {
+            int sum = 0;
+            for (int j = i ; j < nums.length; j++) {
+                sum += nums[j];
+                if (sum == k) {
+                    counter++;
+                }
+            }
+        }
+        return counter;
+    }
+}
