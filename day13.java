fe28ef1a254bc7d7e94c3385047b50688dc243c7
charangoud2004 <k.charangoud2004@gmail.com>
2025-10-13 22:08:33 +0530

    alternative postive and negative numbers

---
+++ day13.java
@@ -0,0 +1,18 @@
+class alternate_pos_and_neg {
+    public int[] rearrangeArray(int[] nums) {
+        ArrayList<Integer> pos = new ArrayList<>();
+        ArrayList<Integer> neg = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] >= 0) {
+                pos.add(nums[i]);
+            } else {
+                neg.add(nums[i]);
+            }
+        }
+        for (int j = 0; j < nums.length / 2; j++) {
+            nums[j * 2] = pos.get(j);
+            nums[j * 2 +1] = neg.get(j);
+        }
+        return nums;
+    }
+}
