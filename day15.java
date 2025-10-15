fd4a360712a58e9dcb419dda6520af9c4198b29e
charangoud2004 <k.charangoud2004@gmail.com>
2025-10-15 23:31:17 +0530

    reverse of array

---
+++ day15.java
@@ -0,0 +1,14 @@
+class Solution {
+  public void reverse(int[] arr, int n) {
+    rev(arr, 0, n - 1); // reverse first n elements
+  }
+
+  /* recursive helper */
+  private void rev(int[] a, int l, int r) {
+    if (l >= r) return;
+    int tmp = a[l];
+    a[l] = a[r];
+    a[r] = tmp;
+    rev(a, l + 1, r - 1);
+  }
+}
