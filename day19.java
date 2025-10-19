78f2eea99ef8081b54467f4b7419d804b100b1b9
charangoud2004 <k.charangoud2004@gmail.com>
2025-10-19 22:51:22 +0530

    upper  bound using binary search

--- day19.java
+++ day19.java
@@ -16,3 +16,23 @@ class lowerBound_in_BinarySearch {
     return ans;
   }
 }
+
+class upperBound_using_BinarySearch {
+  public int upperBound(int[] nums, int x) {
+    int low = 0;
+    int high = nums.length - 1;
+    int ans = nums.length;
+
+    while (low <= high) {
+      int mid = low + (high - low) / 2;
+      if (nums[mid] > x) {
+        ans = mid;
+        high = mid - 1;
+      } else {
+        low = mid + 1;
+      }
+    }
+    return ans;
+  }
+}
+
