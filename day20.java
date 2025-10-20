80590cea37d4be3836379aaef8cdde33584d0b25
charangoud2004 <k.charangoud2004@gmail.com>
2025-10-20 22:17:49 +0530

    finding Floor and Ceiling in an array

--- day20.java
+++ day20.java
@@ -16,3 +16,36 @@ class Search_insert_position {
         return ans;
     }
 }
+
+class Solution {
+  public int[] getFloorAndCeil(int[] nums, int x) {
+    int low = 0;
+    int high = nums.length - 1;
+    int floor = -1;
+    int ceil = -1;
+
+    while (low <= high) {
+      int mid = low + (high - low) / 2;
+
+      // Case 1: Perfect match found
+      if (nums[mid] == x) {
+        floor = nums[mid];
+        ceil = nums[mid];
+        break; // Found the best possible answer, so we can exit the loop
+      }
+
+      // Case 2: Middle element is less than the target
+      if (nums[mid] < x) {
+        floor = nums[mid]; // This is a potential floor
+        low = mid + 1; // Look for a better (larger) floor on the right
+      }
+      // Case 3: Middle element is greater than the target
+      else {
+        ceil = nums[mid]; // This is a potential ceiling
+        high = mid - 1; // Look for a better (smaller) ceiling on the left
+      }
+    }
+
+    return new int[] {floor, ceil};
+  }
+}
