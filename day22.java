d10c1c2adefa9d66c979322184446eca685c9fb3
charangoud2004 <k.charangoud2004@gmail.com>
2025-10-22 22:42:05 +0530

    Search Element in a Rotated Sorted Array using binary search, duplicates are allowed

--- day22.java
+++ day22.java
@@ -29,3 +29,44 @@ class Search_Element_in_a_Rotated_Sorted_Array_1 {
         return -1;
     }
 }
+
+class Search_Element_in_a_Rotated_Sorted_Array_2 {
+    public boolean search(int[] nums, int target) {
+
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target) {
+                return true;
+            }
+            
+            if(nums[low] == nums[mid] == nums[high]){
+                low = low + 1;
+                high = high + 1;
+                continue;
+
+            }
+
+            if (nums[mid] >= nums[low]) {
+
+                if (nums[mid] > target && nums[low] <= target) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+
+            } else {
+
+                if (nums[mid] < target && nums[high] >= target) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+        return false;
+    }
+}
