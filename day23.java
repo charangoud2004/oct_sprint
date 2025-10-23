eaf5e5068c01ed5596f985b5ae9f78b0f37fb6d6
charangoud2004 <k.charangoud2004@gmail.com>
2025-10-23 22:12:04 +0530

    Find out how many times the array is rotated using Binary Search

--- day23.java
+++ day23.java
@@ -15,3 +15,21 @@ class Find_Minimum_in_Rotated_Sorted_Array {
         return nums[low];
     }
 }
+
+class Find_out_how_many_times_the_array_is_rotated {
+  public int findKRotation(ArrayList<Integer> nums) {
+
+    int low = 0;
+    int high = nums.size() - 1;
+
+    while (low < high) {
+      int mid = low + (high - low) / 2;
+      if (nums.get(mid) > nums.get(high)) {
+        low = mid + 1;
+      } else {
+        high = mid;
+      }
+    }
+    return low;
+  }
+}
