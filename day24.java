613d5c949d2fc382cc86dd881a24d2df51e88fdb
charangoud2004 <k.charangoud2004@gmail.com>
2025-10-24 14:04:31 +0530

    Finding Nth root of a numberusing Binary Search

--- day24.java
+++ day24.java
@@ -16,3 +16,22 @@ class Find_square_root_of_a_number {
     return ans;
   }
 }
+
+class Find_Nth_root_of_a_number {
+  public int NthRoot(int N, int M) {
+
+    int low = 1, high = m;
+    while (low <= high) {
+      int mid = low + (high - low) / 2;
+      long ans = 1;
+      for (int i = 0; i < n; i++) {
+        ans *= mid;
+        if (ans > m) break;
+      }
+      if (ans == m) return mid;
+      if (ans < m) low = mid + 1;
+      else high = mid - 1;
+    }
+    return -1;
+  }
+}
